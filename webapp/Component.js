0eda5f232f99d7bd0a4394acf8c5cd8eca61f6fc
Navaneetha Nalamada <navaneethanalamada@gmail.com>
2025-03-14 07:32:15 +0000

    test

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,7 +2,6 @@ sap.ui.define(
     ["sap/suite/ui/generic/template/lib/AppComponent"],
     function (Component) {
         "use strict";
-
         return Component.extend("com.test.testproject.Component", {
             metadata: {
                 manifest: "json"
